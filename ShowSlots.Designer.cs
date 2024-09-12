e51c20f4672fe685f14cbf09817d5ad67f8d964f
dniketh <dniketh@actalentservices.com>
2024-09-12 18:52:08 +0000

    Disconnected Architecture Demo Project

---
+++ ShowSlots.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DisconnectedDemo
+{
+    partial class ShowSlots
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            btnAdd = new Button();
+            btnSave = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(25, 29);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(368, 233);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(416, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(416, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Price";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(416, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Date";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(479, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(479, 102);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(479, 135);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(416, 226);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(541, 226);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 8;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(651, 68);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // ShowSlots
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDelete);
+            Controls.Add(btnSave);
+            Controls.Add(btnAdd);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "ShowSlots";
+            Text = "ShowSlots";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button btnAdd;
+        private Button btnSave;
+        private Button btnDelete;
+    }
+}
---
+++ ShowSlots.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisconnectedDemo
+{
+    public partial class ShowSlots : Form
+    {
+        DataSet dataSet;
+        SqlDataAdapter dataAdapter;
+        SqlCommandBuilder commandBuilder;
+        DataTable dt;
+        public ShowSlots()
+        {
+            unsafe { int* ptr; }; // native code or unmanaged code
+            InitializeComponent();
+            dataSet = new DataSet();
+            dataAdapter = new SqlDataAdapter("select * from slots", @"server=.\sqlexpress;initial catalog=adodemo;user id=sa;password=Pass@123");
+            dataAdapter.Fill(dataSet, "slots");
+            dt = dataSet.Tables["slots"];
+            dt.PrimaryKey = new[] { dt.Columns["id"] }; 
+            commandBuilder = new SqlCommandBuilder(dataAdapter);
+            dataGridView1.DataSource = dataSet.Tables["slots"];
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            dataAdapter.Update(dt);
+            MessageBox.Show("Saved into Database");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataRow row = dt.NewRow();
+            row[1] = textBox1.Text;
+            row[2] = textBox2.Text;
+            row[3] = textBox3.Text;
+            dt.Rows.Add(row);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataRow row = dt.Rows.Find(1);
+            dt.Rows.Remove(row);
+        }
+        
+    }
+}
